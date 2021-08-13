576645a9be6a7d928e1da238676d929d04abd331
jshji252 <jasonseunghoonji@gmail.com>
2021-08-13 15:38:24 +1000

    Fixed Time
    Time was confused between UTC and brisbane time @.@

--- api/api.py
+++ api/api.py
@@ -10,14 +10,17 @@ def get_header():
     def epocher(time_int):
         # Turns the time info from BOM into epoch time
         s=str(time_int)
-        return 1000*datetime.datetime(int(s[:4]),int(s[4:6]),int(s[6:8]),int(s[8:10]),int(s[10:12])).timestamp()
+        epoch=1000*(datetime.datetime(int(s[:4]),int(s[4:6]),int(s[6:8]),int(s[8:10]),int(s[10:12])) - datetime.datetime(1970,1,1,0,0)).total_seconds()
+        print(s)
+        print(epoch)
+        return epoch
     api_address = request.args.get("api_address")
     r = requests.get(api_address)
     try:
         jsn = r.json()
         header = jsn["observations"]["header"][0]
         weather_data = jsn["observations"]["data"]
-        datas = [{"date": epocher(data["local_date_time_full"]), "temp": data["apparent_t"], "sort_order": data["sort_order"]} for data in weather_data]
+        datas = [{"date": epocher(data["aifstime_utc"]), "temp": data["apparent_t"], "sort_order": data["sort_order"]} for data in weather_data]
         return jsonify(datas)
     except:
         print(f"Could not resolve BOM's data. Status code: {r.status_code}!")
